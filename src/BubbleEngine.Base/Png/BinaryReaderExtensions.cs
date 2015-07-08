a143608279ee75c2d131adc95305ba3e4855ef4f
CallumDev <callum.mcging@gmail.com>
2015-07-08 21:15:19 +0930

    Implemented PngLoader. Generic loading interface to come

---
+++ src/BubbleEngine.Base/Png/BinaryReaderExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace BubbleEngine
+{
+	unsafe static class BinaryReaderExtensions
+	{
+		static readonly byte[] bytes = new byte[4];
+		public static int ReadInt32BE(this BinaryReader reader)
+		{
+			reader.Read (bytes, 0, 4);
+			if (BitConverter.IsLittleEndian) {
+				int x = (bytes [0] << 24) | (bytes [1] << 16) | (bytes [2] << 8) | bytes [3];
+				return x;
+			} else {
+				fixed (byte *ptr = bytes) {
+					return *(int*)ptr;
+				}
+			}
+		}
+	}
+}
+
