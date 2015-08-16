c1c97a6cd26d25ed9a8eb62bb93de9d4ed308fe0
CallumDev <callum.mcging@gmail.com>
2015-08-17 08:29:50 +0930

    More font work

--- src/BubbleEngine.Base/Font.cs
+++ src/BubbleEngine.Base/Font.cs
@@ -52,12 +52,17 @@ namespace BubbleEngine
 		}
 
 		IntPtr facePtr;
+		IntPtr size26d6;
 		FontContext context;
 		List<Texture> pages = new List<Texture>();
-
+		Dictionary<uint, GlyphInfo> glyphs = new Dictionary<uint, GlyphInfo>();
+		int currentX = 0;
+		int currentY = 0;
+		int lineHeight;
 		public Font (FontContext ctx, string filename, float size)
 		{
 			context = ctx;
+			size26d6 = FTMath.To26Dot6 (size);
 			if (!File.Exists (filename))
 				throw new FileNotFoundException ("Font not found " + filename, filename);
 
@@ -67,11 +72,14 @@ namespace BubbleEngine
 			
 			FT.FT_Set_Char_Size (facePtr,
 				FTMath.To26Dot6 (0),
-				FTMath.To26Dot6 (size),
+				size26d6,
 				0,
 				96
 			);
-
+			//get metrics
+			var faceRec = Marshal.PtrToStructure<FT.FaceRec> (facePtr);
+			var szRec = Marshal.PtrToStructure<FT.SizeRec> (faceRec.size);
+			lineHeight = (int)FTMath.From26Dot6 (szRec.metrics.height);
 			pages.Add (new Texture (
 				PAGE_SIZE, PAGE_SIZE
 			));
@@ -80,11 +88,102 @@ namespace BubbleEngine
 			for (int i = 32; i < 127; i++) {
 				AddCharacter ((uint)i);
 			}
+
 		}
-			
-		void AddCharacter(uint codepoint)
+		GlyphInfo GetGlyph(uint codepoint)
 		{
-			
+			if (!glyphs.ContainsKey (codepoint))
+				AddCharacter (codepoint);
+			return glyphs [codepoint];
+		}
+		int lineMax = 0;
+		unsafe void AddCharacter(uint codepoint)
+		{
+			if (codepoint == (uint)'\t') {
+				var spaceGlyph = GetGlyph ((uint)' ');
+				glyphs.Add (codepoint, new GlyphInfo (spaceGlyph.AdvanceX * 4, spaceGlyph.AdvanceY, spaceGlyph.CharIndex, spaceGlyph.Face));
+				return;
+			}
+			IntPtr face;
+			uint index;
+			if (!GetFace (codepoint, out index, out face)) {
+				if (codepoint == (uint)'?')
+					throw new Exception ("Font does not have required ASCII characters");
+				var qmGlyph = GetGlyph ((uint)'?');
+				glyphs.Add (codepoint, qmGlyph);
+				return;
+			}
+			FT.FT_Load_Glyph (face, index, FT.FT_LOAD_DEFAULT | FT.FT_LOAD_TARGET_NORMAL);
+			var faceRec = Marshal.PtrToStructure<FT.FaceRec> (face);
+			FT.FT_Render_Glyph (faceRec.glyph, FT.FT_RENDER_MODE_NORMAL);
+			var glyphRec = Marshal.PtrToStructure<FT.GlyphSlotRec> (faceRec.glyph);
+			if (glyphRec.bitmap.width == 0 || glyphRec.bitmap.rows == 0) {
+				glyphs.Add (codepoint,
+					new GlyphInfo (
+						(int)Math.Ceiling(FTMath.From26Dot6 (glyphRec.advance.x)),
+						(int)Math.Ceiling(FTMath.From26Dot6 (glyphRec.advance.y)),
+						index,
+						face
+					)
+				);
+			} else {
+				var colors = new ByteColor[glyphRec.bitmap.width * glyphRec.bitmap.rows];
+				if (glyphRec.bitmap.pixel_mode == 2) {
+					byte* data = (byte*)glyphRec.bitmap.buffer;
+					for (int i = 0; i < glyphRec.bitmap.width * glyphRec.bitmap.rows; i++) {
+						//TODO: 4 bytes used for 1 byte of alpha? investigate compression with GL_RED and shader.
+						colors [i] = new ByteColor (255, 255, 255, data [i]);
+					}
+				} else {
+					throw new NotImplementedException ();
+				}
+				if (currentX + glyphRec.bitmap.width > PAGE_SIZE) {
+					currentX = 0;
+					currentY += lineMax;
+					lineMax = 0;
+				}
+				if (currentY + glyphRec.bitmap.rows > PAGE_SIZE) {
+					currentX = 0;
+					currentY = 0;
+					lineMax = 0;
+					pages.Add (new Texture (PAGE_SIZE, PAGE_SIZE));
+					//TODO: new font page! should probably log this
+				}
+				lineMax = (int)Math.Max (lineMax, glyphRec.bitmap.rows);
+				var rect = new Rectangle (currentX, currentY, glyphRec.bitmap.width, glyphRec.bitmap.rows);
+				var tex = pages [pages.Count - 1];
+				tex.SetData (colors, rect);
+				currentX += glyphRec.bitmap.width;
+
+			}
+		}
+		bool GetFace(uint codepoint, out uint index, out IntPtr face)
+		{
+			//use our font!
+			index = FT.FT_Get_Char_Index (facePtr, codepoint);
+			if (index != 0) {
+				face = facePtr;
+				return true;
+			}
+			//try fallback font
+			if (context.Fallback == IntPtr.Zero) {
+				face = IntPtr.Zero;
+				return false;
+			}
+			FT.FT_Set_Char_Size (context.Fallback,
+				FTMath.To26Dot6 (0),
+				size26d6,
+				0,
+				96
+			);
+			index = FT.FT_Get_Char_Index (context.Fallback, codepoint);
+			if (index != 0) {
+				face = context.Fallback;
+				return true;
+			}
+			//none of the fonts have this mysterious character
+			face = IntPtr.Zero;
+			return false;
 		}
 	}
 }
--- src/BubbleEngine.Base/Interop/FT.cs
+++ src/BubbleEngine.Base/Interop/FT.cs
@@ -4,6 +4,12 @@ namespace BubbleEngine
 {
 	static class FT
 	{
+		public const int FT_RENDER_MODE_NORMAL = 0;
+		public const int FT_LOAD_DEFAULT = 0;
+		//define FT_LOAD_TARGET_( x ) ( (FT_Int32)( (x) & 15) << 16 )
+		//define FT_LOAD_TARGET_NORMAL = FT_LOAD_TARGET_(FT_RENDER_MODE_NORMAL)
+		public const int FT_LOAD_TARGET_NORMAL = (FT_RENDER_MODE_NORMAL & 15) << 16;
+
 		//The fields do get assigned to, just through reflection.
 		#pragma warning disable 0649
 
@@ -73,6 +79,178 @@ namespace BubbleEngine
 			}
 			InteropHelper.LoadFunctions (typeof(FT), (x) => loader.GetProcAddress (library, x));
 			Loaded = true;
+		}
+
+		/// <summary>
+		/// Internally represents a Face.
+		/// </summary>
+		/// <remarks>
+		/// Refer to <see cref="Face"/> for FreeType documentation.
+		/// </remarks>
+		[StructLayout(LayoutKind.Sequential)]
+		internal class FaceRec
+		{
+			internal IntPtr num_faces;
+			internal IntPtr face_index;
+
+			internal IntPtr face_flags;
+			internal IntPtr style_flags;
+
+			internal IntPtr num_glyphs;
+
+			internal IntPtr family_name;
+			internal IntPtr style_name;
+
+			internal int num_fixed_sizes;
+			internal IntPtr available_sizes;
+
+			internal int num_charmaps;
+			internal IntPtr charmaps;
+
+			internal GenericRec generic;
+
+			internal BBox bbox;
+
+			internal ushort units_per_EM;
+			internal short ascender;
+			internal short descender;
+			internal short height;
+
+			internal short max_advance_width;
+			internal short max_advance_height;
+
+			internal short underline_position;
+			internal short underline_thickness;
+
+			internal IntPtr glyph;
+			internal IntPtr size;
+			internal IntPtr charmap;
+
+			private IntPtr driver;
+			private IntPtr memory;
+			private IntPtr stream;
+
+			private IntPtr sizes_list;
+			private GenericRec autohint;
+			private IntPtr extensions;
+
+			private IntPtr @internal;
+
+			internal static int SizeInBytes { get { return Marshal.SizeOf(typeof(FaceRec)); } }
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct GenericRec
+		{
+			internal IntPtr data;
+			internal IntPtr finalizer;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct BBox
+		{
+			IntPtr xMin, yMin;
+			IntPtr xMax, yMax;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal class GlyphSlotRec
+		{
+			internal IntPtr library;
+			internal IntPtr face;
+			internal IntPtr next;
+			internal uint reserved;
+			internal GenericRec generic;
+
+			internal GlyphMetricsRec metrics;
+			internal IntPtr linearHoriAdvance;
+			internal IntPtr linearVertAdvance;
+			internal FT_Vector advance;
+
+			internal uint format;
+
+			internal BitmapRec bitmap;
+			internal int bitmap_left;
+			internal int bitmap_top;
+
+			internal OutlineRec outline;
+
+			internal uint num_subglyphs;
+			internal IntPtr subglyphs;
+
+			internal IntPtr control_data;
+			internal IntPtr control_len;
+
+			internal IntPtr lsb_delta;
+			internal IntPtr rsb_delta;
+
+			internal IntPtr other;
+
+			private IntPtr @internal;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct GlyphMetricsRec
+		{
+			internal IntPtr width;
+			internal IntPtr height;
+
+			internal IntPtr horiBearingX;
+			internal IntPtr horiBearingY;
+			internal IntPtr horiAdvance;
+
+			internal IntPtr vertBearingX;
+			internal IntPtr vertBearingY;
+			internal IntPtr vertAdvance;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct BitmapRec
+		{
+			internal int rows;
+			internal int width;
+			internal int pitch;
+			internal IntPtr buffer;
+			internal short num_grays;
+			internal byte pixel_mode;
+			internal byte palette_mode;
+			internal IntPtr palette;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct OutlineRec
+		{
+			internal short n_contours;
+			internal short n_points;
+
+			internal IntPtr points;
+			internal IntPtr tags;
+			internal IntPtr contours;
+
+			internal int flags;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal class SizeRec
+		{
+			internal IntPtr face;
+			internal GenericRec generic;
+			internal SizeMetricsRec metrics;
+			private IntPtr @internal;
+		}
+
+		[StructLayout(LayoutKind.Sequential)]
+		internal struct SizeMetricsRec
+		{
+			internal ushort x_ppem;
+			internal ushort y_ppem;
+
+			internal IntPtr x_scale;
+			internal IntPtr y_scale;
+			internal IntPtr ascender;
+			internal IntPtr descender;
+			internal IntPtr height;
+			internal IntPtr max_advance;
 		}
 	}
 }
